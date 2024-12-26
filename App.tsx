cfd3acb6faf0dfbc39f29c3b34da086160296a19
Rakib Roni <rakibronicse@gmail.com>
2024-12-26 23:06:53 +0100

    add navigation library

--- App.tsx
+++ App.tsx
@@ -3,25 +3,24 @@ import { View, Text, StyleSheet } from 'react-native';
 
 import LittleLemonHeader from './components/LittleLemonHeader';
 import LittleLemonFooter from './components/LittleLemonFooter';
-import MenuItems from './components/MenuItems';
+import MenuScreen from './components/MenuItems';
 import SectionListMenuItems from './components/SectionListMenuItems';
 import FeedbackForm from './components/FeedbackForm';
 import LoginScreen from './LoginScreen';
 import WelcomeScreen from './WelcomeScreen';
 import ImageList from './components/ImageList';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
 export default function App() {
+  const Stack = createNativeStackNavigator();
   return (
-    <>
-      <View style={styles.container}>
-        <LittleLemonHeader />
-        <LoginScreen />
-        <WelcomeScreen />
-      </View>
-      <View style={styles.footerContainer}>
-        <LittleLemonFooter />
-      </View>
-    </>
+    <NavigationContainer>
+    <Stack.Navigator initialRouteName="Welcome">
+      <Stack.Screen name="Welcome" component={WelcomeScreen} />
+      <Stack.Screen name="Menu" component={MenuScreen} />
+    </Stack.Navigator>
+  </NavigationContainer>
   );
 }
 
