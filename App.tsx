cdc69dcb500eaa7856f7ab871c4e77908697bb00
Rakib Roni <rakibronicse@gmail.com>
2024-12-27 00:12:19 +0100

    add navigation

--- App.tsx
+++ App.tsx
@@ -3,24 +3,29 @@ import { View, Text, StyleSheet } from 'react-native';
 
 import LittleLemonHeader from './components/LittleLemonHeader';
 import LittleLemonFooter from './components/LittleLemonFooter';
-import MenuScreen from './components/MenuItems';
-import SectionListMenuItems from './components/SectionListMenuItems';
-import FeedbackForm from './components/FeedbackForm';
-import LoginScreen from './LoginScreen';
 import WelcomeScreen from './WelcomeScreen';
-import ImageList from './components/ImageList';
+import LoginScreen from './LoginScreen';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
+const Stack = createNativeStackNavigator();
+
 export default function App() {
-  const Stack = createNativeStackNavigator();
   return (
-    <NavigationContainer>
-    <Stack.Navigator initialRouteName="Welcome">
-      <Stack.Screen name="Welcome" component={WelcomeScreen} />
-      <Stack.Screen name="Menu" component={MenuScreen} />
-    </Stack.Navigator>
-  </NavigationContainer>
+    <>
+      <NavigationContainer>
+        <View style={styles.container}>
+          <LittleLemonHeader />
+          <Stack.Navigator initialRouteName="Login">
+            <Stack.Screen name="Welcome" component={WelcomeScreen} />
+            <Stack.Screen name="Login" component={LoginScreen} />
+          </Stack.Navigator>
+        </View>
+        <View style={styles.footerContainer}>
+          <LittleLemonFooter />
+        </View>
+      </NavigationContainer>
+    </>
   );
 }
 
