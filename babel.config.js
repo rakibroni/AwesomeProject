b99e72ff71f89fa2cd7c71af9f9cea14452f498d
Rakib Roni <rakibronicse@gmail.com>
2024-12-29 00:43:37 +0100

    remove comment

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   presets: ['module:@react-native/babel-preset'],
   plugins: [
-    'react-native-reanimated/plugin', // Ensure this is last
+    'react-native-reanimated/plugin', 
   ],
 };
